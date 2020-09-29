5989d8b5bbd25674ee6ca9250f9d2b2310518da4
JacksonDek <jackson_dekeyser@outlook.com>
2020-09-29 07:29:02 -0500

    create the counter and displayed the results

--- Multiple3And5/Program.cs
+++ Multiple3And5/Program.cs
@@ -10,7 +10,16 @@ namespace Multiple3And5
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                {
+                    sum = sum + i;
+                }
 
+            }
+            Console.WriteLine(sum);
         }
     }
 }
